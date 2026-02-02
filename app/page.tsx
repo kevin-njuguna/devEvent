715fe586b010f032a57687573fe7ac54310c25c2
kevin-njuguna <kevy.kamah@gmail.com>
2026-02-02 13:51:39 +0300

    implement api routes, server actions and caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import ExploreBtn from "@/components/ExploreBtn"
 import EventCard from "@/components/EventCard"
-import { events } from "@/lib/constants"
+import { IEvent } from "@/database/event.model"
+import { cacheLife } from "next/cache"
+//import { events } from "@/lib/constants"
 
-const page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+const page = async () => {
+  'use cache'
+  cacheLife('hours')
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const {events} = await response.json()
   return (
     <section>
       <h1 className="text-center">The Hub For Every Tech Event</h1>
@@ -11,7 +19,7 @@ const page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured</h3>
         <ul className="events">
-          {events.map((event) => (
+          {events && events.length > 0 &&events.map((event: IEvent) => (
             <li key={event.title}>
               <EventCard {...event}/>
             </li>
