3c0eaf278635a653f35b077a08ad69bddf75ea04
kevin-njuguna <kevy.kamah@gmail.com>
2026-02-02 22:36:49 +0300

    fix deployment errors by making some changes to the pages

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,15 +1,18 @@
-import {Suspense} from "react";
-import EventDetails from "@/components/EventDetails";
+import { Suspense } from "react"
+import EventDetails from "@/components/EventDetails"
 
-const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
-    const slug = params.then((p) => p.slug);
-
-    return (
-        <main>
-            <Suspense fallback={<div>Loading...</div>}>
-                <EventDetails params={slug} />
-            </Suspense>
-        </main>
-    )
+const EventDetailsPage = async ({
+  params,
+}: {
+  params: { slug: string }
+}) => {
+  return (
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EventDetails params={params.slug} />
+      </Suspense>
+    </main>
+  )
 }
+
 export default EventDetailsPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,41 @@
 import ExploreBtn from "@/components/ExploreBtn"
 import EventCard from "@/components/EventCard"
 import { IEvent } from "@/database/event.model"
-import { cacheLife } from "next/cache"
 
+export const dynamic = "force-dynamic"
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+const Page = async () => {
+  const response = await fetch("/api/events")
 
-const page = async () => {
-  'use cache'
-  cacheLife('hours')
-  const response = await fetch(`${BASE_URL}/api/events`)
-  const {events} = await response.json()
+  if (!response.ok) {
+    return (
+      <section>
+        <h1 className="text-center">The Hub For Every Tech Event</h1>
+        <p className="text-center mt-5">
+          Hackathons, Meetups, and Conferences, All in One place
+        </p>
+        <p className="text-center mt-10 text-red-500">
+          Events unavailable
+        </p>
+      </section>
+    )
+  }
+
+  const { events } = await response.json()
+
   return (
     <section>
       <h1 className="text-center">The Hub For Every Tech Event</h1>
-      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One place</p>
-      <ExploreBtn/>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One place
+      </p>
+      <ExploreBtn />
       <div className="mt-20 space-y-7">
         <h3>Featured</h3>
         <ul className="events">
-          {events && events.length > 0 &&events.map((event: IEvent) => (
-            <li key={event.title}>
-              <EventCard {...event}/>
+          {events?.map((event: IEvent) => (
+            <li key={event._id}>
+              <EventCard {...event} />
             </li>
           ))}
         </ul>
@@ -30,4 +44,4 @@ const page = async () => {
   )
 }
 
-export default page
+export default Page
