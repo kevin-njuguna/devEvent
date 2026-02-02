93760f676be709d8a38c8756310cdde533042bf3
kevin-njuguna <kevy.kamah@gmail.com>
2026-02-02 22:28:36 +0300

    fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 const page = async () => {
   'use cache'
   cacheLife('hours')
-  const response = await fetch(`${BASE_URL}api/events`)
+  const response = await fetch(`${BASE_URL}/api/events`)
   const {events} = await response.json()
   return (
     <section>
